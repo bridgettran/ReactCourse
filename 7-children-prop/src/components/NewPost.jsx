f66c049bd1c6789400ddf55ba6cbd3fad402ba9d
bridgettran <dtran5@academic.rrc.ca>
2025-01-28 14:28:51 -0600

    Add childrent prop example

---
+++ 7-children-prop/src/components/NewPost.jsx
@@ -0,0 +1,20 @@
+import classes from './NewPost.module.css';
+
+function NewPost(props) {
+  return (
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+
+        // prop name can be what we want
+        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
+      </p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
+      </p>
+    </form>
+  );
+}
+
+export default NewPost;
