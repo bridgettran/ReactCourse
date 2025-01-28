571a9f63b98019f9304b886d99bdff38da5e3ff1
bridgettran <dtran5@academic.rrc.ca>
2025-01-28 17:06:17 -0600

    Add update state based on the previous state example

---
+++ 10-update-state-based-on-previous-state/src/components/PostsList.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import Post from './Post';
+import NewPost from './NewPost';
+import Modal from './Modal';
+import classes from './PostsList.module.css';
+
+function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    // here is the way to update the state if it depend on the previous statesnapshot
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
+        </Modal>
+      )}
+      <ul className={classes.posts}>
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
+  );
+}
+
+export default PostsList;
