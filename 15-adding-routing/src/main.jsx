ac58f6cea10eacd9687889546c1b72cd74659c6e
bridgettran <dtran5@academic.rrc.ca>
2025-02-04 13:29:06 -0600

    Add working with mutible routes code

---
+++ 15-adding-routing/src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import App from './App'
+import NewPost from './components/NewPost';
+import './index.css'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/create-post', element: <NewPost /> }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
